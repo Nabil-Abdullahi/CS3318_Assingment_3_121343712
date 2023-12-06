7a7af996293b9c4c77671d1b6309ef13818f654f
nabil <121343723@umail.ucc.ie>
2023-12-06 14:06:49 +0000

    Created test file

---
+++ src/test/java/j3a/tester.java
@@ -0,0 +1,10 @@
+package j3a;
+
+import org.junit.jupiter.api.Test;
+
+public class tester {
+    @Test
+    public void noOfColorInPalette(){
+        ColourTable palette = new ColourTable(pal);
+    }
+}
